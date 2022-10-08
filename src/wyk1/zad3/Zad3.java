952ebebb3f013e3c9dabb0ffe22ef91c7d8dbabd
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-10-08 14:29:51 +0200

    zad 3

---
+++ src/wyk1/zad3/Zad3.java
@@ -0,0 +1,13 @@
+package wyk1.zad3;
+
+public class Zad3 {
+    public static void main(String[] args) {
+        int x =5;
+        int y = ++x;
+        int z = x++;
+        System.out.printf("y = %s\n", y);
+        System.out.printf("z = %s\n", z);
+        System.out.printf("x = %s\n", x);
+
+    }
+}
