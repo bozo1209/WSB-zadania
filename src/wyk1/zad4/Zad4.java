5304922bd492b905cd2c9087538f2efa9da85a6d
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-10-08 14:47:38 +0200

    zad 4

---
+++ src/wyk1/zad4/Zad4.java
@@ -0,0 +1,17 @@
+package wyk1.zad4;
+
+public class Zad4 {
+
+    public static void main(String[] args) {
+        int number;
+
+        for (number = 1; number < 3; number++){
+            System.out.printf("number %s is %s", number, checkIfOddOrEven(number));
+            System.out.println();
+        }
+    }
+
+    public static String checkIfOddOrEven(int number){
+        return (number % 2 == 0) ? "even" : "odd";
+    }
+}
