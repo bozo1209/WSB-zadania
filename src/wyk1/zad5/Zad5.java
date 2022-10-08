6dd6522bb3949cb30305b8c346a88a4ee16d7cb9
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-10-08 15:14:07 +0200

    zad 4

---
+++ src/wyk1/zad5/Zad5.java
@@ -0,0 +1,23 @@
+package wyk1.zad5;
+
+public class Zad5 {
+    public static void main(String[] args) {
+        int age = 5;
+        String animal = "dog";
+
+        System.out.printf("you are %s %s", oldOrYoung(age), animalType(animal));
+
+    }
+
+    private static String oldOrYoung(int age){
+        return age > 8 ? "old" : "young";
+    }
+
+    private static String animalType(String animal){
+        return switch (animal.toUpperCase()){
+            case "DOG" -> "dog";
+            case "CAT" -> "cat";
+            default -> "";
+        };
+    }
+}
