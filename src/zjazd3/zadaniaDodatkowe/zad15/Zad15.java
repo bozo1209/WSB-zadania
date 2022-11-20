d10af1bc0a9c3d19040a11843fa6d9ad75e30064
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-11-20 11:55:55 +0100

    zadanie dodatkowe 15

--- src/zjazd3/zadaniaDodatkowe/zad15/Zad15.java
+++ src/zjazd3/zadaniaDodatkowe/zad15/Zad15.java
@@ -1,6 +1,8 @@
 package zjazd3.zadaniaDodatkowe.zad15;
 
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Zad15 {
 
@@ -40,6 +42,7 @@ public class Zad15 {
         System.out.println(function.apply("test"));
         System.out.println(function2.apply("test"));
 
-
+        String collect = Stream.of("test").map(function).map(function2).collect(Collectors.joining(" "));
+        System.out.println(collect);
     }
 }
