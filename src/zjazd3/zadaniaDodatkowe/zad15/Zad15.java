608d543200d83d127f884c9615003d8e8f3b6474
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-11-20 11:49:13 +0100

    zadanie dodatkowe 15

--- src/zjazd3/zadaniaDodatkowe/zad15/Zad15.java
+++ src/zjazd3/zadaniaDodatkowe/zad15/Zad15.java
@@ -1,5 +1,7 @@
 package zjazd3.zadaniaDodatkowe.zad15;
 
+import java.util.function.Function;
+
 public class Zad15 {
 
     public static void main(String[] args) {
@@ -20,6 +22,24 @@ public class Zad15 {
 
         System.out.println(upperCase.upperCase("test"));
         System.out.println(letterSpaceSeparated.letterSpaceSeparated("test"));
+
+
+        Function<String, String> function = String::toUpperCase;
+        Function<String, String> function2 = s -> {
+            StringBuilder sb = new StringBuilder();
+            char[] chars = s.toCharArray();
+            for (int i = 0; i < chars.length; i++){
+                sb.append(chars[i]);
+                if (!(i == chars.length - 1)){
+                    sb.append(" ");
+                }
+            }
+            return sb.toString();
+        };
+
+        System.out.println(function.apply("test"));
+        System.out.println(function2.apply("test"));
+
 
     }
 }
