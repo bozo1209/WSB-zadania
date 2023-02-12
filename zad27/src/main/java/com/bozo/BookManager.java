f1a9a2b35da45d0343b9bd4e42f4c4a33f1eab34
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-02-12 08:17:17 +0100

    p55 zad 27

--- zad27/src/main/java/com/bozo/BookManager.java
+++ zad27/src/main/java/com/bozo/BookManager.java
@@ -23,20 +23,35 @@ public class BookManager implements AutoCloseable {
     }
 
     public void addBook(Book book){
+//        session.getTransaction().begin();
+//        System.out.println("1");
+//        Optional<Book> book1 = getBook(book.getIsbn());
+//        if (book1.isPresent()){
+//            System.out.println("2");
+//            book1.get().setTitle(book.getTitle());
+//            session.update(book1.get());
+//            session.getTransaction().commit();
+//            return;
+//        }
+//        System.out.println("3");
+//        session.save(book);
+//        System.out.println("4");
+//        session.getTransaction().commit();
+
         session.getTransaction().begin();
-        System.out.println("1");
-        Optional<Book> book1 = getBook(book.getIsbn());
-        if (book1.isPresent()){
-            System.out.println("2");
-            book1.get().setTitle(book.getTitle());
-            session.update(book1.get());
-            session.getTransaction().commit();
-            return;
-        }
-        System.out.println("3");
-        session.save(book);
-        System.out.println("4");
+        getBook(book.getIsbn()).ifPresentOrElse(
+                b -> {
+                    b.setTitle(book.getTitle());
+                    session.update(b);
+                },
+                () -> session.save(book));
         session.getTransaction().commit();
+
+//        session.getTransaction().begin();
+//        Book book2 = getBook(book.getIsbn()).orElse(book);
+//        book2.setTitle(book.getTitle());
+//        session.saveOrUpdate(book2);
+//        session.getTransaction().commit();
     }
 
     public void deleteBooks(){
--- zad27/src/main/java/com/bozo/Main.java
+++ zad27/src/main/java/com/bozo/Main.java
@@ -14,16 +14,26 @@ public class Main {
             book1.setIsbn(1);
 
             Book book2 = new Book();
-            book2.setTitle("book2");
+            book2.setTitle("book2a");
             book2.setIsbn(2);
 
             Book book3 = new Book();
-            book3.setTitle("book3");
+            book3.setTitle("book3 aaa");
             book3.setIsbn(3);
+
+            Book book4 = new Book();
+            book4.setTitle("book4");
+            book4.setIsbn(4);
+
+            Book book5 = new Book();
+            book5.setTitle("book5");
+            book5.setIsbn(5);
 
             bookManager.addBook(book1);
             bookManager.addBook(book2);
             bookManager.addBook(book3);
+            bookManager.addBook(book4);
+            bookManager.addBook(book5);
 
             System.out.printf("Check (2): %s%n", bookManager.getBooks());
             bookManager.getBooks().forEach(book -> System.out.println("title " + book.getTitle()));
